c9c1e83096d9db2929f3d37b7a9b662a04be68a8
gabrielpasini <gabrielpasini@outlook.com.br>
2022-10-22 18:02:03 -0300

    :pencil: implementa filtro por code e ajusta retornos/erros

--- src/controllers/observationController.js
+++ src/controllers/observationController.js
@@ -1,28 +1,54 @@
 const express = require("express");
 const router = express.Router();
 const observations = require("../mocks/observations.json");
+const patients = require("../mocks/patients.json");
 
 router.get("/", async (req, res) => {
   const { patient, code } = req.query;
   try {
     if (!patient)
-      return res.status(500).send("É necessário informar um ID de paciente.");
-    const searchedPatient = observations.find(
+      return res.status(400).send({
+        message: "É necessário informar um ID de paciente.",
+      });
+
+    const searchedPatient = patients.find((item) => item.id === patient);
+    if (!searchedPatient)
+      return res.status(400).send({
+        message: "Este ID de paciente não existe.",
+      });
+
+    const observationsByPatient = observations.filter(
       (item) => item.subject.reference.split("/")[1] === patient
     );
-    if (!searchedPatient)
-      return res.status(500).send("O ID do paciente não existe.");
+    if (!observationsByPatient.length)
+      return res.status(200).send({
+        message: "Este paciente não possui nenhuma leitura de sinal vital.",
+        patient: searchedPatient,
+        observations: [],
+      });
 
-    // TODO: fazer filtro por code
+    if (code) {
+      const patientObservationsByCode = observationsByPatient.filter((item) =>
+        item.code.coding.some((item) => item.code === code)
+      );
+
+      return res.status(200).send({
+        message: "Sinais Vitais carregados com sucesso.",
+        patient: searchedPatient,
+        observations: patientObservationsByCode,
+      });
+    }
 
     return res.status(200).send({
-      query: req.query,
+      message: "Sinais Vitais carregados com sucesso.",
       patient: searchedPatient,
+      observations: observationsByPatient,
     });
   } catch (err) {
-    return res
-      .status(400)
-      .send({ error: err, message: "Erro ao atualizar o modo!" });
+    return res.status(500).send({
+      message: "Erro ao buscar por sinais vitais, tente novamente.",
+      error: err,
+    });
   }
 });
 
--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -6,18 +6,24 @@ router.get("/", async (req, res) => {
   const { id } = req.query;
   try {
     if (!id)
-      return res.status(500).send("É necessário informar um ID de paciente.");
+      return res.status(200).send({
+        patient: patients,
+      });
+
     const searchedPatient = patients.find((item) => item.id === id);
     if (!searchedPatient)
-      return res.status(500).send("O ID do paciente não existe.");
+      return res.status(400).send({
+        message: "Este ID de paciente não existe.",
+      });
+
     return res.status(200).send({
-      query: req.query,
       patient: searchedPatient,
     });
   } catch (err) {
-    return res
-      .status(400)
-      .send({ error: err, message: "Erro ao atualizar o modo!" });
+    return res.status(500).send({
+      error: err,
+      message: "Erro ao buscar por pacientes, tente novamente.",
+    });
   }
 });
 
